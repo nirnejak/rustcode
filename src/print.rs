ab47862f265721ecb96da1f02e1cf4e94ab5fd9e
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2020-10-28 23:17:42 +0530

    Print

---
+++ src/print.rs
@@ -0,0 +1,27 @@
+pub fn run() {
+  // Print to console
+  println!("Hello from the print.rs file");
+
+  // Basic Formatting
+  println!("I am {} and I live in {}", "Jitendra", "Bangalore");
+  println!("I am {} years old", 22);
+
+  // Positional Arguments
+  println!(
+    "{0} is from {1} and {0} likes to {2}",
+    "Jitendra", "Bangalore", "Code"
+  );
+
+  // Named Arguments
+  println!(
+    "{name} likes to play {activity}",
+    name = "Jitendra",
+    activity = "baseball"
+  );
+
+  // Placeholder traits
+  println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
+
+  // Placeholder fro debug trait
+  println!("{:?}", (12, true, "hello"))
+}
